54f8cd49030ac5d3992a7e0c5623ffe8e1dce4ce
U-suika\wailoktam <wailoktam@gmail.com>
2017-02-04 17:45:06 +0900

    change for when

--- sharWikiQTctrl.py
+++ sharWikiQTctrl.py
@@ -48,9 +48,9 @@ if __name__ == '__main__':
     trainList = []
     devList = []
     testList = []
-    trainQT = open("trainQTCt_where", "wb")
-    devQT = open("devQTCt_where", "wb")
-    testQT = open("testQTCt_where", "wb")
+    trainQT = open("trainQTCt_when", "wb")
+    devQT = open("devQTCt_when", "wb")
+    testQT = open("testQTCt_when", "wb")
     inFiles = {'train': train, 'dev': dev, 'test': test}
     outLists = {'train':trainList, 'dev': devList, 'test':testList}
     outFiles = {'train':trainQT, 'dev': devQT, 'test':testQT}
@@ -62,7 +62,7 @@ if __name__ == '__main__':
     aList = []
     qList = {}
 
-    qTerms = ['where']
+    qTerms = ['when']
     #    qTerms = ['who', 'when', 'where', 'what']
 
     for file in inFiles.keys():
